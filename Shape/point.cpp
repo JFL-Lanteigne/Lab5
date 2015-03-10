6d78c47971d0e71e5775c248290b00c40bd4083e
Jean-Francois Laverdiere Lanteigne <jfl.lanteigne@gmail.com>
2015-03-10 10:36:55 -0400

    PointsTests : exception lancée quand x est négatif
    L'exception est bien lancée et le test passe

--- Shape/point.cpp
+++ Shape/point.cpp
@@ -9,17 +9,21 @@ Point::Point( double _x, double _y ) :
 	x(_x), 
 	y(_y)
 {	
-	
+	if (x < 0) throw invalid_argument("x ne doit pas être plus petit que 0");
+	if (y < 0) throw invalid_argument("y ne doit pas être plus petit que 0");
 }
 
 bool Point::operator == (const Point & point) const
 {
-	throw logic_error("Not Implmented yet");
+	if (x != point.x || y != point.y)
+	{
+		return false;
+	}
 	return true;
 }
 
 bool Point::operator != (const Point & point) const
 {
-	throw logic_error("Not Implmented yet");
-	return false; 
+	if (operator==(point)) return false;
+	return true;
 }
