ca7086801c0249c0371c9f34516e77c21f0293be
Jean-Francois Laverdiere Lanteigne <jfl.lanteigne@gmail.com>
2015-03-10 10:45:54 -0400

    PointsTests : operateur == comparer 2 points (=/=)
    retourne faux comme prévu, test réussi

--- UnitTests/PointsTests.cpp
+++ UnitTests/PointsTests.cpp
@@ -49,18 +49,18 @@ namespace UnitTests
 			Assert::IsTrue(exceptionThrown);
 		}
 
-		//TEST_METHOD(given_2_points_to_compare_when_they_are_not_equal_then_should_return_false)
-		//{
-		//	//arrange
-		//	Point point1(0, 1);
-		//	Point point2(0, 2);
-		//	
-		//	//action 
-		//	bool compareResult = point1 == point2;
-		//	
-		//	//assert
-		//	Assert::IsFalse(compareResult);
-		//}
+		TEST_METHOD(given_2_points_to_compare_when_they_are_not_equal_then_should_return_false)
+		{
+			//arrange
+			Point point1(0, 1);
+			Point point2(0, 2);
+			
+			//action 
+			bool compareResult = point1 == point2;
+			
+			//assert
+			Assert::IsFalse(compareResult);
+		}
 
 		//TEST_METHOD(given_2_points_to_compare_when_they_are_equal_then_should_return_true)
 		//{
