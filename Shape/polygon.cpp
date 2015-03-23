a1fa77186e208fbb8ea7e2a009f1ff44b4b72a01
Jean-Francois Laverdiere Lanteigne <jfl.lanteigne@gmail.com>
2015-03-23 19:31:41 -0400

    PolygonTests : draw Polygon should draw lines
    le polygon est bel et bien dessiné, test réussi.

--- Shape/polygon.cpp
+++ Shape/polygon.cpp
@@ -15,7 +15,21 @@ Polygon::~Polygon()
 
 void Polygon::draw()
 {
+	if (point.size() < 3) throw runtime_error("Il doit y avoir au moins 3 points dans un Polygone");
 
+	windowAPI->setDrawingColor(lineColor);
+
+	for (unsigned int i = 0; i < point.size(); i++)
+	{
+		if (i != (point.size() - 1))
+		{
+			windowAPI->drawLine(point.at(i), point.at(i + 1));
+		}
+		else
+		{
+			windowAPI->drawLine(point.at(i), point.at(i - (point.size() - 1)));
+		}
+	}
 }
 
 void Polygon::add(Point& _point)
@@ -42,6 +56,7 @@ bool Polygon::pointIsOnPreviousLine(Point& _point)
 
 	for (unsigned int i = 0; i < point.size() - 1; i++)
 	{
+		bool slopeUndefined = false;
 		float x1 = static_cast<float>(point.at(i).x);
 		float x2 = static_cast<float>(point.at(i + 1).x);
 		float y1 = static_cast<float>(point.at(i).y);
@@ -49,8 +64,16 @@ bool Polygon::pointIsOnPreviousLine(Point& _point)
 		float px = static_cast<float>(_point.x);
 		float py = static_cast<float>(_point.y);
 
-		slope = (y2 - y1) / (x2 - x1);
-		yIntercept = y1 - slope * x1;
+		if (x2 - x1 != 0)
+		{
+			slope = (y2 - y1) / (x2 - x1);
+			yIntercept = y1 - slope * x1;
+		}
+		else
+		{
+			slope = NULL;
+			slopeUndefined = true;
+		}
 
 		if (x1 < x2)
 		{
@@ -74,9 +97,19 @@ bool Polygon::pointIsOnPreviousLine(Point& _point)
 			bottom = y2;
 		}
 
-		if (slope * px + yIntercept > (py - 0.01) && slope * px + yIntercept < (py + 0.01))
+		if (slopeUndefined == true)
 		{
-			if (px >= left && px <= right && py <= top && py >= bottom) return true;
+			if (px > (py - 0.01) && px < (py + 0.01))
+			{
+				if (px >= left && px <= right && py <= top && py >= bottom) return true;
+			}
+		}
+		else
+		{
+			if (slope * px + yIntercept >(py - 0.01) && slope * px + yIntercept < (py + 0.01))
+			{
+				if (px >= left && px <= right && py <= top && py >= bottom) return true;
+			}
 		}
 	}
 	return false;
--- UnitTests/PolygonTests.cpp
+++ UnitTests/PolygonTests.cpp
@@ -44,46 +44,46 @@ namespace UnitTests
 			//Assert
 			Assert::IsTrue(exceptionThrown);
 		}
-		//
-		//TEST_METHOD(add_point_to_a_polygon_that_cross_preivous_lines_should_throw_excpetion)
-		//{
-		//	//Arrange
-		//	polygon->add(Point(25, 25));
-		//	polygon->add(Point(25, 75));
-		//	polygon->add(Point(75, 100));
+		
+		TEST_METHOD(add_point_to_a_polygon_that_cross_preivous_lines_should_throw_excpetion)
+		{
+			//Arrange
+			polygon->add(Point(25, 25));
+			polygon->add(Point(25, 75));
+			polygon->add(Point(75, 100));
 
-		//	//Action
-		//	bool exceptionThrown = false;
+			//Action
+			bool exceptionThrown = false;
 
-		//	try
-		//	{
-		//		polygon->add(Point(0, 50));
-		//	}
-		//	catch (runtime_error ex)
-		//	{
-		//		exceptionThrown = true;
-		//	}
+			try
+			{
+				polygon->add(Point(0, 50));
+			}
+			catch (runtime_error ex)
+			{
+				exceptionThrown = true;
+			}
 
-		//	//Assert
-		//	Assert::IsTrue(exceptionThrown);
-		//}
+			//Assert
+			Assert::IsTrue(exceptionThrown);
+		}
 
-		//TEST_METHOD(draw_Poloygon_should_draw_lines_on_windowAPI)
-		//{
-		//	//Arrange	
-		//	const int NOMBRE_DE_LIGNE_DESSINEES = 4;
+		TEST_METHOD(draw_Poloygon_should_draw_lines_on_windowAPI)
+		{
+			//Arrange	
+			const int NOMBRE_DE_LIGNE_DESSINEES = 4;
 
-		//	polygon->add(Point(100, 100));
-		//	polygon->add(Point(150, 200));
-		//	polygon->add(Point(250, 250));
-		//	polygon->add(Point(175, 175));
+			polygon->add(Point(100, 100));
+			polygon->add(Point(150, 200));
+			polygon->add(Point(250, 250));
+			polygon->add(Point(175, 175));
 
-		//	//Action
-		//	polygon->draw();
+			//Action
+			polygon->draw();
 
-		//	//Assert
-		//	Assert::IsTrue(fakeWindowAPI->setDrawingColor_hasBeenCalled());
-		//	Assert::AreEqual(NOMBRE_DE_LIGNE_DESSINEES, fakeWindowAPI->drawLine_getnumberOfCall());
-		//}
+			//Assert
+			Assert::IsTrue(fakeWindowAPI->setDrawingColor_hasBeenCalled());
+			Assert::AreEqual(NOMBRE_DE_LIGNE_DESSINEES, fakeWindowAPI->drawLine_getnumberOfCall());
+		}
 	};
 }
